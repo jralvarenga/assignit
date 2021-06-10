873a8a41d2289b5bf287878b5f9c36c5bfaaa59e
jralvarenga <jralvarenga161@gmail.com>
2021-06-10 15:54:57 -0600

    Minor chnage

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { theme } from './src/services/theme'
-import './src/styles/global.css'
+import './src/styles/global.css' 
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
