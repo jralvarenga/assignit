915f1cc624e3a47024962c975ebabdada3f51b12
jralvarenga <jralvarenga161@gmail.com>
2021-06-13 23:51:00 -0600

    Minor change

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-theme-material-ui`,
-    `gatsby-plugin-material-ui`,
+    //`gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
