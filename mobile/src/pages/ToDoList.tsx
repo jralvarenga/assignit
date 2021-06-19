87e09befae69fbdf124d7c9f208833aa1ab239e7
jralvarenga <jralvarenga161@gmail.com>
2021-06-19 00:51:34 -0600

    Add new task design

--- mobile/src/pages/ToDoList.tsx
+++ mobile/src/pages/ToDoList.tsx
@@ -1,13 +1,37 @@
 import { useTheme } from '@react-navigation/native'
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Text, IconButton } from 'react-native-paper'
+import React, { useState } from 'react'
+import { StyleSheet, View, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native'
+import { Text, IconButton, TextInput } from 'react-native-paper'
 import { Theme } from 'react-native-paper/lib/typescript/types'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ColorPicker from '../components/ColorPicker'
+
+const colors = [
+  { id: "1", color: "#a4bdfc" },
+  { id: "2", color: "#7ae7bf" },
+  { id: "3", color: "#dbadff" },
+  { id: "4", color: "#ff887c" },
+  { id: "5", color: "#fbd75b" },
+  { id: "6", color: "#ffb878" },
+  { id: "7", color: "#46d6db" },
+  //{ id: "8", color: "#e1e1e1" },
+  { id: "9", color: "#5484ed" },
+  { id: "10", color: "#51b749" },
+  { id: "11", color: "#dc2127" },
+  { id: "12", color: "#ededed" },
+]
 
 const ToDoListScreen = () => {
   const theme = useTheme()
   const styles = styleSheet(theme)
+  const [addNewTask, setAddNewTask] = useState(false)
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  const [newTaskColor, setNewTaskColor] = useState(colors[10])
+
+  const taskColorHandler = (color: any) => {
+    setNewTaskColor(color)
+    setShowColorPicker(false)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -16,16 +40,51 @@ const ToDoListScreen = () => {
         <Text style={[styles.font, {fontSize: 30}]}>
           To Do List
         </Text>
-        <IconButton
-          icon="plus"
+        {/*<IconButton
+          icon={addIcon}
           color={theme.colors.text}
           size={30}
           style={{ backgroundColor: theme.colors.card }}
-          onPress={() => console.log('Pressed')}
+          onPress={() => {
+            setAddNewTask(!addNewTask)
+            showHideHandler()
+          }}
+        />*/}
+      </View>
+
+      <View style={[styles.addNewContainer, { flex: 1 }]}>
+        <TextInput
+          mode="outlined"
+          label="New task name"
+          style={{ width: '50%' }}
+          theme={{ colors: { primary: newTaskColor.color } }}
+        />
+        <TouchableOpacity
+          onPress={() => setShowColorPicker(true)}
+          activeOpacity={0.7}
+          style={[styles.colorChooser, {backgroundColor: newTaskColor.color}]}
+        />
+        <IconButton
+          icon='clock-outline'
+          color={theme.colors.text}
+          size={32}
+          style={{ backgroundColor: newTaskColor.color }}
+        />
+        <IconButton
+          icon='plus'
+          color={theme.colors.text}
+          size={32}
+          style={{ backgroundColor: newTaskColor.color }}
         />
       </View>
 
       <View style={styles.listContainer}></View>
+
+      <ColorPicker
+        visible={showColorPicker}
+        setVisible={setShowColorPicker}
+        colorPickerHandler={taskColorHandler}
+      />
 
     </SafeAreaView>
   )
@@ -43,11 +102,37 @@ const styleSheet = (theme: Theme | any) => StyleSheet.create({
     fontSize: 16
   },
   titleContainer: {
-    flex: 1,
+    flex: 0.7,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     width: '95%',
+  },
+  addNewContainer: {
+    width: '95%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  taskNameContainer: {
+    width: '70%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  colorChooser: {
+    width: 47,
+    height: 47,
+    borderRadius: 100,
+    marginBottom: 5,
+    borderColor: theme.colors.surface
+  },
+  addNewTask: {
+    width: 120,
+    height: 40,
+    elevation: 0,
   },
   listContainer: {
     flex: 6,
